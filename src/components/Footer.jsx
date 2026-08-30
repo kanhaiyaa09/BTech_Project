26075237c4c9e21f74f7440e88bb36463c811a15
kanhaiyaa09 <kanhaiyaa.agrawal.btech2023@sitnagpur.siu.edu.in>
2026-08-31 03:34:08 +0530

    Phase 1: rebrand footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   const year = new Date().getFullYear();
 
-  return <footer>{`Copyright © Web3 Certify ${year}`}</footer>;
+  return <footer>{`Copyright © AcademiChain Vault ${year}`}</footer>;
 };
 
 export default Footer;
